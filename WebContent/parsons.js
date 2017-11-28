551cfb8634b76f1bccbeac67e6b94a1c0fb609bf
adilsonv77 <adilsonv77@gmail.com>
2017-11-28 13:40:51 -0200

    Ajustando a tela

---
+++ WebContent/parsons.js
@@ -0,0 +1,27 @@
+function initBlockly() {
+	/*
+	var blocklyArea = document.getElementById('blockly');
+	var blocklyDiv = document.getElementById('blocklyDiv');
+	var game = Blockly.inject(blocklyDiv,
+	    {toolbox: document.getElementById('toolbox')});
+	var onresize = function(e) {
+		    // Compute the absolute coordinates and dimensions of blocklyArea.
+		    var element = blocklyArea;
+		    var x = 0;
+		    var y = 0;
+		    do {
+		      x += element.offsetLeft;
+		      y += element.offsetTop;
+		      element = element.offsetParent;
+		    } while (element);
+		    // Position blocklyDiv over blocklyArea.
+		    blocklyDiv.style.left = x + 'px';
+		    blocklyDiv.style.top = y + 'px';
+		    blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+		    blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+	};
+	window.addEventListener('resize', onresize, false);
+	onresize();
+	Blockly.svgResize(game);
+	*/
+}
